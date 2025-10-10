4ee25559df9775799bc2ef46581253c939c38296
bricorne <bricepoitevineau@gmail.com>
2025-10-10 14:14:20 +0200

    task 0

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * wildcmp - Compares two strings and returns 1 if they can be
+ * considered identical, otherwise returns 0.
+ * @s1: The first string.
+ * @s2: The second string, which can contain the special character *.
+ *
+ * Return: 1 if the strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
